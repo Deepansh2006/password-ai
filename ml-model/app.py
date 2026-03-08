3f8b841b4aa151861a89b001a2d30c66685f2638
Deepansh2006 <your_email@example.com>
2026-03-08 11:51:19 +0530

    Fixed Flask server for Render deployment

--- ml-model/app.py
+++ ml-model/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import re
+import os
 
 app = Flask(__name__)
 
@@ -39,5 +40,7 @@ def predict():
     })
 
 
+# Important for Render deployment
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))   # Render provides PORT automatically
+    app.run(host="0.0.0.0", port=port)
